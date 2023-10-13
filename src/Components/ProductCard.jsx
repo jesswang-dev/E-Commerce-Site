c50369b8aa3d29edb5855694a8e4742757b17714
Jessica Wang <xuecen.wang@gmail.com>
2023-10-12 21:14:32 -0400

    solved conflicts manually

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -5,11 +5,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-<<<<<<< HEAD
 import { Button, CardActionArea, CardActions, Grid, Container} from "@mui/material";
-=======
-import { Button, CardActionArea, CardActions, Grid } from "@mui/material";
->>>>>>> 3371743847e5c960ffb095d2d57562e0c0a7500f
 
 export default function ProductCard() {
   const db = getFirestore(firebaseApp);
@@ -38,7 +34,6 @@ export default function ProductCard() {
 
   return (
     <>
-<<<<<<< HEAD
       <Container>
         <Grid sx={{ maxWidth: 1200 }} container spacing={2}>
           <Grid item xs={12}>
@@ -75,42 +70,6 @@ export default function ProductCard() {
           </Grid>
         </Grid>
       </Container>
-=======
-      <Grid sx={{ maxWidth: 1200 }} container spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justifyContent="center" spacing={2}>
-            {productList.map((item) => {
-              return (
-                <Grid key={item.id} item>
-                  <Card sx={{ maxWidth: 200 }}>
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="100"
-                        image={item.image}
-                        alt={item.name}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h6" component="div">
-                          {item.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          {item.brand}
-                        </Typography>
-                        <Typography variant="body2">{`$${item.price}.00`}</Typography>
-                      </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                      <Button size="small">Add to Cart</Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Grid>
-      </Grid>
->>>>>>> 3371743847e5c960ffb095d2d57562e0c0a7500f
     </>
   );
 }
--- src/Components/RecommendedProduct.jsx
+++ src/Components/RecommendedProduct.jsx
@@ -11,11 +11,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-<<<<<<< HEAD
 import { CardActionArea, Grid, Container} from "@mui/material";
-=======
-import { CardActionArea } from "@mui/material";
->>>>>>> 3371743847e5c960ffb095d2d57562e0c0a7500f
 
 export default function RecommendedProduct() {
   const db = getFirestore(firebaseApp);
@@ -50,7 +46,6 @@ export default function RecommendedProduct() {
   console.log('recommended', productList);
   return (
     <>
-<<<<<<< HEAD
       <Container>
         <Grid sx={{ maxWidth: 1200 }} container spacing={2}>
           <Grid item xs={12}>
@@ -83,35 +78,6 @@ export default function RecommendedProduct() {
           </Grid>
         </Grid>
       </Container>
-=======
-      <ul>
-        {
-        productList.map((item) => {
-          return (
-            <li key={item.id}>
-              <Card sx={{ maxWidth: 300 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={item.image}
-                    alt={item.name}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {item.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {item.brand}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </li>
-          );
-        })}
-      </ul>
->>>>>>> 3371743847e5c960ffb095d2d57562e0c0a7500f
     </>
   );
 }
