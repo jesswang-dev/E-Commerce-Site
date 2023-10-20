8172ec30d49288b62f16d0a97509022f2f43ed3b
Jessica Wang <xuecen.wang@gmail.com>
2023-10-20 15:33:51 -0400

    completed expand menu in UserAccount Components, finished styling, new components to view account profile waiting to be added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import ShoppingCart from "./Components/ShoppingCart";
+// import UserAccount from "./Components/UserAccount";
+// import TestAccount from "./Components/TestAccount";
 
 function App() {
   return (
